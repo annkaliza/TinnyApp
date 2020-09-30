36bd2500ff08bc4ebf82f5ddeae6562f0cd63502
Kaliza Anna <annkaliza@gmail.com>
2020-09-30 10:43:41 +0000

    add functionality to add new url

--- express_server.js
+++ express_server.js
@@ -37,9 +37,17 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  console.longURL(req.params.shortURL, "=========")
+  const longURL = urlDatabase[req.params.shortURL].longURL;
+  res.redirect(longURL);
+});
+
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const shortURL = generateRandomStrings();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 
